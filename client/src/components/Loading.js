411fe2d9dec5e5af177a8fbc65c50ba75480dd31
jung <eugenejung04@gmail.com>
2023-10-09 03:21:42 -0400

    update Loading.js

---
+++ client/src/components/Loading.js
@@ -0,0 +1,12 @@
+// shown to user when resume is pending
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div className='app'>
+            <h1>Loading, please wait...</h1>
+        </div>
+    );
+};
+
+export default Loading;
