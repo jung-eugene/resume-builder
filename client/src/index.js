be570c6809b1eaaeb4866a452b1c8f7ac8536e4a
jung <eugenejung04@gmail.com>
2023-10-10 10:51:03 -0400

    submit images via forms

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const multer = require("multer");
+const path = require("path");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +14,25 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// enables Node.js to serve the contents of an uploads folder
+// refers to static files such as images, CSS, JS files
+app.use("/uploads", express.static("uploads"));
+
+// storage var gives us full control of images
+// func stores images in the upload folder and renames images to upload time
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+// passes config to Multer and set size limit of 5MB of images
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 5 },
+});
+
 reportWebVitals();
