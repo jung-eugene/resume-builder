afe56fa5f4c5a8629a78d1e8cb780c4e27450e0a
jung <eugenejung04@gmail.com>
2023-10-10 11:09:20 -0400

    add images to uploads folder

--- server/index.js
+++ server/index.js
@@ -16,3 +16,23 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+// accepts all form inputs from React App
+// upload.single("headshotImage") func adds the image uploaded via the form
+// to the uploads folder
+app.post("/resume/create", upload.single("headshotImage"), async (req, res) => {
+    const {
+        fullName,
+        currentPosition,
+        currentLength,
+        currentTechnologies,
+        workHistory,
+    } = req.body;
+
+    console.log(req.body);
+
+    res.json({
+        message: "Request successful!",
+        data: {},
+    });
+});
