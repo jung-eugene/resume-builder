4f2e059fee1d4ff54b44427a8519860c8d7bb31c
jung <eugenejung04@gmail.com>
2023-10-12 02:29:46 -0400

    create react state

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,17 @@ import Home from "./components/Home";
 import Resume from "./components/Resume";
 
 const App = () => {
+    // React state that holds result sent from Node.js server
+    const [result, setResult] = useState({});
+
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/resume' element={<Resume />} />
+                    {/*-- setResult updates the value of result once the form is submitted and request is successful --*/}
+                    <Route path='/' element={<Home setResult = {setResult}/>} />
+                    {/*-- result contains the response retrieved from server --*/}
+                    <Route path='/resume' element={<Resume result = {result}/>} />
                 </Routes>
             </BrowserRouter>
         </div>
