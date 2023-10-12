ca446c1608dc266b301c0b6cd5041750bc14ca69
jung <eugenejung04@gmail.com>
2023-10-12 02:37:18 -0400

    preview resume result

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -3,16 +3,78 @@ import React from "react";
 import ErrorPage from "./ErrorPage";
 
 const Resume = ({ result }) => {
+    // func that replaces the new line with a break tag
+    const replaceWithBr = (string) => {
+        return string.replace(/\n/g, "<br />")
+    };
+
+    // return error page if result object is empty
     if (JSON.stringify(result) === "{}") {
         return <ErrorPage />;
     }
 
-    const handlePrint = () => alert("Print Successful!");
+    // preview result within React app
+    const handlePrint = () => alert("Printing...");
     return (
         <>
             <button onClick={handlePrint}>Print Page</button>
-            <main className='container'>
-                <p>Hello!</p>
+            <main className='container' ref={componentRef}>
+                <header className='header'>
+                    <div>
+                        <h1>{result.fullName}</h1>
+                        <p className='resumeTitle headerTitle'>
+                            {result.currentPosition} ({result.currentTechnologies})
+                        </p>
+                        <p className='resumeTitle'>
+                            {result.currentLength}year(s) work experience
+                        </p>
+                    </div>
+                    <div>
+                        <img
+                            src={result.image_url}
+                            alt={result.fullName}
+                            className='resumeImage'
+                        />
+                    </div>
+                </header>
+                <div className='resumeBody'>
+                    <div>
+                        <h2 className='resumeBodyTitle'>PROFILE SUMMARY</h2>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: replaceWithBr(result.objective),
+                            }}
+                            className='resumeBodyContent'
+                        />
+                    </div>
+                    <div>
+                        <h2 className='resumeBodyTitle'>WORK HISTORY</h2>
+                        {result.workHistory.map((work) => (
+                            <p className='resumeBodyContent' key={work.name}>
+                                <span style={{ fontWeight: "bold" }}>{work.name}</span> -{" "}
+                                {work.position}
+                            </p>
+                        ))}
+                    </div>
+                    <div>
+                        <h2 className='resumeBodyTitle'>JOB PROFILE</h2>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: replaceWithBr(result.jobResponsibilities),
+                            }}
+                            className='resumeBodyContent'
+                        />
+                    </div>
+                    <div>
+                        <h2 className='resumeBodyTitle'>JOB RESPONSIBILITIES</h2>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: replaceWithBr(result.keypoints),
+                            }}
+                            className='resumeBodyContent'
+                        />
+                    </div>
+                </div>
             </main>
         </>
     );
